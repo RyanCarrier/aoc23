fbc401a0f22145bf1258f09cfe61d4b21659beca
Ryan Carrier <ryan.j.carrier@gmail.com>
2023-12-01 16:23:22 +1000

    part 1

--- src/day1.rs
+++ src/day1.rs
@@ -8,50 +8,35 @@ pub const DAY1: Problem = Problem {
 };
 
 pub fn part1(lines: Vec<String>) -> String {
-    let mut result = [false; 2020];
+    let mut total = 0;
     for l in lines {
-        let n: i32 = l.parse().unwrap();
-        if result[2020 - n as usize] {
-            return format!("{}", (2020 - n) * n);
-        } else {
-            result[n as usize] = true;
+        let mut num = 0;
+        for c in l.chars() {
+            if c.is_numeric() {
+                num = c.to_string().parse::<i32>().unwrap() * 10;
+                break;
+            }
         }
+        for c in l.chars().rev() {
+            if c.is_numeric() {
+                num += c.to_string().parse::<i32>().unwrap();
+                break;
+            }
+        }
+        total += num;
     }
-    return "".to_owned();
+    format!("{}", total)
 }
 
 pub fn part2(lines: Vec<String>) -> String {
-    let target = 2020;
-    let mut nums: Vec<i32> = lines
-        .into_iter()
-        .filter(|x| !x.is_empty())
-        .map(|x| x.parse::<i32>().unwrap())
-        .collect();
-    nums.sort();
-    for i in 0..nums.len() {
-        let mut left = i + 1;
-        let mut right = nums.len() - 1;
-        while left < right {
-            let sum = nums[i] + nums[left] + nums[right];
-            if sum == target {
-                return format!("{}", nums[i] * nums[left] * nums[right]);
-            } else if sum < target {
-                left += 1;
-            } else {
-                right -= 1;
-            }
-        }
-    }
     return "".to_owned();
 }
 pub fn test_data() -> Option<String> {
     Some(
-        "1721
-979
-366
-299
-675
-1456"
+        "1abc2
+pqr3stu8vwx
+a1b2c3d4e5f
+treb7uchet"
             .to_owned(),
     )
 }
