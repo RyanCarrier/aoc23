9ff4108f019902b6a6e3e30c76c515b2593669a6
Ryan Carrier <ryan.j.carrier@gmail.com>
2023-12-06 15:54:53 +1000

    day6 part2 and small cleanup

--- src/days/day6.rs
+++ src/days/day6.rs
@@ -15,6 +15,8 @@ struct Race {
 }
 impl Race {
     fn min_charge_times(&self) -> (usize, usize) {
+        //find roots of distance=charge(duration-charge)
+        //ie; 0=-charge^2+duration*charge-distance
         let discriminant: isize =
             (self.duration * self.duration) as isize - 4 * (self.distance + 1) as isize;
         //+1 to distance because we need to BEAT the distance and time, not match it
@@ -26,6 +28,7 @@ impl Race {
         let (mut b, mut a) = ((half_t + half_root).floor(), (half_t - half_root).ceil());
         //because i just want a to be smaller
         if a.is_sign_negative() {
+            //this might actually never happen with these cases
             a = b;
         } else if b.is_sign_negative() {
             b = a;
@@ -35,39 +38,51 @@ impl Race {
     }
     fn all_winning_times(&self) -> Vec<usize> {
         let (a, b) = self.min_charge_times();
-        let result: Vec<usize> = (a..=b).collect();
-        // println!(
-        //     "{} {} : {} {} - {:?}",
-        //     self.duration,
-        //     self.distance,
-        //     a,
-        //     b,
-        //     result.clone()
-        // );
-        result
+        (a..=b).collect()
     }
 }
 
 pub fn part1(lines: &Vec<String>) -> String {
     let data = import(lines);
-    let lens: Vec<usize> = data
+    let product_of_len: usize = data
         .races
         .iter()
         .map(|x| x.all_winning_times().len())
-        .collect();
-    // println!("{:?}", lens);
-    let product_of_len = lens.iter().product::<usize>();
+        .product::<usize>();
     format!("{}", product_of_len)
 }
 
 pub fn part2(lines: &Vec<String>) -> String {
-    // let data = import(lines);
-    "".to_owned()
+    let data = import_pt2(lines);
+    let ways_to_win = data.races[0].all_winning_times().len();
+    format!("{}", ways_to_win)
 }
 pub fn test_data() -> String {
     "Time:      7  15   30
 Distance:  9  40  200"
         .to_owned()
+}
+fn import_pt2(lines: &Vec<String>) -> Day6Data {
+    //probably not needed half the time
+    let duration = lines[0]
+        .split(":")
+        .last()
+        .expect("Duration (1st) line should have :")
+        .trim()
+        .replace(" ", "")
+        .parse::<usize>()
+        .expect("Duration should be a number");
+    let distance = lines[1]
+        .split(":")
+        .last()
+        .expect("Distance (2nd) line should have :")
+        .trim()
+        .replace(" ", "")
+        .parse::<usize>()
+        .expect("Duration should be a number");
+    Day6Data {
+        races: vec![Race { duration, distance }],
+    }
 }
 
 fn import(lines: &Vec<String>) -> Day6Data {
