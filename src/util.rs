2215a4db150a9b89809e49e370a3848a002d56dd
Ryan Carrier <ryan.j.carrier@gmail.com>
2023-12-11 22:53:23 +1000

    add dijkstra cause it's going to be day12 i can feel it

--- src/util.rs
+++ src/util.rs
@@ -1,6 +1,13 @@
-use std::{fs, path::Path, sync::Arc, time::Duration};
+use std::{collections::BinaryHeap, fs, path::Path, sync::Arc, time::Duration};
 
 use reqwest::{cookie::Jar, Url};
+#[derive(Clone, Copy)]
+pub struct Problem {
+    pub day: usize,
+    pub part1: fn(input: &Vec<String>) -> String,
+    pub part2: fn(input: &Vec<String>) -> String,
+    pub test_data: Option<fn() -> &'static str>,
+}
 #[allow(dead_code)]
 pub static TRANSFORMS: [[isize; 2]; 8] = [
     [-1, -1],
@@ -52,10 +59,106 @@ pub fn get_input_data(year: usize, day: usize) -> Vec<String> {
     body.split('\n').map(|x| x.to_owned()).collect()
 }
 
-#[derive(Clone, Copy)]
-pub struct Problem {
-    pub day: usize,
-    pub part1: fn(input: &Vec<String>) -> String,
-    pub part2: fn(input: &Vec<String>) -> String,
-    pub test_data: Option<fn() -> &'static str>,
+#[allow(dead_code)]
+pub struct Graph {
+    pub verticies: usize,
+    pub arcs: Vec<Vec<VectorArc>>,
+}
+pub struct VectorArc {
+    pub from: usize,
+    pub to: usize,
+    pub weight: usize,
+}
+#[allow(dead_code)]
+impl VectorArc {
+    pub fn new(from: usize, to: usize, weight: usize) -> Self {
+        Self { from, to, weight }
+    }
+}
+#[derive(Copy, Clone, Eq, Debug)]
+#[allow(dead_code)]
+struct Vertex {
+    index: usize,
+    dist: usize,
+}
+impl PartialEq for Vertex {
+    fn eq(&self, other: &Self) -> bool {
+        self.dist == other.dist
+    }
+}
+impl Ord for Vertex {
+    fn cmp(&self, other: &Self) -> std::cmp::Ordering {
+        self.dist.cmp(&other.dist).reverse()
+        // other.dist.cmp(&self.dist)
+    }
+}
+
+impl PartialOrd for Vertex {
+    fn partial_cmp(&self, other: &Self) -> Option<std::cmp::Ordering> {
+        Some(self.cmp(&other))
+    }
+}
+#[allow(dead_code)]
+impl Graph {
+    pub fn dijkstra(&self, from: usize, to: usize) -> usize {
+        let mut dist = vec![usize::MAX; self.verticies];
+        let mut pq: BinaryHeap<Vertex> = BinaryHeap::new();
+        dist[from] = 0;
+        pq.push(Vertex {
+            index: from,
+            dist: 0,
+        });
+        while let Some(next) = pq.pop() {
+            println!("{:?}", next);
+            if next.index == to {
+                return next.dist;
+            }
+            for arc in self.arcs[next.index].iter() {
+                let new_dist = next.dist + arc.weight;
+                println!(
+                    "{}=={}->{} newdist {} dist[] {}",
+                    next.index, arc.from, arc.to, new_dist, dist[arc.to]
+                );
+                if new_dist < dist[arc.to] {
+                    dist[arc.to] = new_dist;
+                    pq.push(Vertex {
+                        index: arc.to,
+                        dist: new_dist,
+                    });
+                }
+            }
+            println!("{:?}", pq);
+        }
+        0
+    }
+}
+//test for dijkstra
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_dijkstra() {
+        let graph = Graph {
+            verticies: 6,
+            arcs: vec![
+                vec![VectorArc::new(0, 1, 1), VectorArc::new(0, 2, 12)],
+                vec![
+                    VectorArc::new(1, 2, 9),
+                    VectorArc::new(1, 3, 3),
+                    VectorArc::new(1, 2, 1),
+                ],
+                vec![VectorArc::new(2, 4, 5)],
+                vec![
+                    VectorArc::new(3, 2, 4),
+                    VectorArc::new(3, 4, 13),
+                    VectorArc::new(3, 5, 15),
+                ],
+                vec![VectorArc::new(4, 5, 4)],
+                vec![],
+            ],
+        };
+        assert_eq!(graph.dijkstra(0, 5), 11);
+        assert_eq!(graph.dijkstra(0, 2), 2);
+    }
 }
